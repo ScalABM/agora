53647a5c86fb9d9680d3925540eb28810d534ae1
David R. Pugh <drobert.pugh@gmail.com>
2015-11-06 10:37:40 +0000

    Added a stub ExchangeLike trait.

--- src/main/scala-2.11/exchanges/ExchangeLike.scala
+++ src/main/scala-2.11/exchanges/ExchangeLike.scala
@@ -15,5 +15,43 @@ limitations under the License.
 */
 package exchanges
 
+import akka.actor.{Props, Actor}
+import markets.MarketLike
+import markets.clearing.ClearingMechanismLike
+import markets.orders.OrderLike
+import markets.settlement.SettlementMechanismLike
+import markets.tradables.Tradable
 
-trait ExchangeLike
+import scala.collection.immutable
+
+
+/** Base trait for all `ExchangeLike` actors.
+  *
+  * @note An `ExchangeLike` represents a collection of `MarketLike` actors that share a common settlement
+  *       mechanism.
+  */
+trait ExchangeLike {
+  this: Actor =>
+
+  def clearingMechanisms: immutable.Seq[ClearingMechanismLike]
+
+  def tradables: immutable.Seq[Tradable]
+
+  /* Settlement mechanism is a child of the ExchangeLike. */
+  val settlementMechanism = ???
+
+  /* Create a market actor for each security in tickers. */
+  val markets = ???
+
+  def receive: Receive = {
+    case order: OrderLike => ???
+    case AddMarket(clearingMechanism, tradable) =>  // add a market to the exchange
+    case RemoveMarket(tradable) =>  // remove a market from the exchange
+    case _ => ???
+  }
+
+  case class AddMarket(clearingMechanism: ClearingMechanismLike, tradable: Tradable)
+
+  case class RemoveMarket(tradable: Tradable)
+
+}
