c10c1d232cc2efd251cf4711c6ec12613b0ef51e
David R. Pugh <drobert.pugh@gmail.com>
2015-11-07 20:56:23 +0000

    Added limit order classes.

--- src/main/scala-2.11/markets/orders/LimitBidOrder.scala
+++ src/main/scala-2.11/markets/orders/LimitBidOrder.scala
@@ -1,8 +1,39 @@
+/*
+Copyright 2015 David R. Pugh, J. Doyne Farmer, and Dan F. Tang
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package markets.orders
 
-/**
- * Created by drpugh on 11/7/15.
- */
-class LimitBidOrder {
+import akka.actor.ActorRef
+import markets.tradables.Tradable
+
+
+case class LimitBidOrder(issuer: ActorRef,
+                         limitPrice: Long,
+                         quantity: Long,
+                         tradable: Tradable) extends BidOrderLike with LimitOrderLike {
+
+  require(limitPrice > 0, "Price must be strictly positive.")
+
+  require(quantity > 0, "Quantity must be strictly positive.")
+
+  def crosses(order: AskOrderLike): Boolean = {
+    ???
+  }
+
+  def split(newQuantity: Long): LimitBidOrder = {
+    LimitBidOrder(issuer, limitPrice, newQuantity, tradable)
+  }
 
 }
