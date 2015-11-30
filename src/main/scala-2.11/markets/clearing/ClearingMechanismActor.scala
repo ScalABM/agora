dde5edea485c7cd1c02a41d9b9294855f99a2adc
David R. Pugh <drobert.pugh@gmail.com>
2015-11-30 14:04:30 +0000

    Fixed some typos in docs.

--- src/main/scala-2.11/markets/clearing/ClearingMechanismActor.scala
+++ src/main/scala-2.11/markets/clearing/ClearingMechanismActor.scala
@@ -21,18 +21,15 @@ import markets.BaseActor
 import markets.clearing.engines.MatchingEngineLike
 import markets.orders.Order
 
-import scala.util.Failure
-
 
 /** Actor for modeling market clearing mechanisms.
   *
   * A `ClearingMechanismActor` actor should receive orders and fill them using its matching
-  * engine. Filled
-  * orders are then sent to a `SettlementMechanismLike` actor for further processing.
+  * engine. `Fills` are then sent to a `SettlementMechanismLike` actor for further processing.
   * @param matchingEngine A `ClearingMechanismActor` has a matching engine for forming prices and
   *                       quantities
   * @param settlementMechanism A `ClearingMechanismActor` has access to some settlement mechanism
-  *                            that it uses to process fills orders into successful transactions.
+  *                            that it uses to process fills into successful transactions.
   */
 class ClearingMechanismActor(val matchingEngine: MatchingEngineLike,
                              val settlementMechanism: ActorRef) extends BaseActor
