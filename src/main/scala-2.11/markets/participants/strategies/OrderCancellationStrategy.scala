bf461490c98e9e7414afd93802bdb0f97c6c1819
David R. Pugh <drobert.pugh@gmail.com>
2016-01-13 13:35:29 +0000

    Minor modifications.

--- src/main/scala-2.11/markets/participants/strategies/OrderCancellationStrategy.scala
+++ src/main/scala-2.11/markets/participants/strategies/OrderCancellationStrategy.scala
@@ -1,12 +1,49 @@
 package markets.participants.strategies
 
+import akka.actor.{ActorRef, Scheduler}
+
 import markets.orders.Order
 
 import scala.collection.mutable
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.FiniteDuration
 
 
 trait OrderCancellationStrategy {
 
+  def scheduler: Scheduler
+
   def cancelOneOf[T <: mutable.Iterable[Order]](outstandingOrders: T): Option[Order]
+
+  /** Schedule a specific message to be sent to some market (possibly after some delay).
+    *
+    * @param delay
+    * @param receiver
+    * @param message
+    * @param executionContext
+    */
+  def scheduleOnce(delay: FiniteDuration,
+                   receiver: ActorRef,
+                   message: Any)
+                  (implicit executionContext: ExecutionContext): Unit = {
+    scheduler.scheduleOnce(delay, receiver, message)(executionContext)
+  }
+
+  /** Schedule a specific message to be sent repeatedly to some market participant (possibly
+    * after some delay).
+    *
+    * @param initialDelay
+    * @param interval
+    * @param participant
+    * @param reminder
+    * @param executionContext
+    */
+  def schedule(initialDelay: FiniteDuration,
+               interval: FiniteDuration,
+               participant: ActorRef,
+               reminder: Any)
+              (implicit executionContext: ExecutionContext): Unit = {
+    scheduler.schedule(initialDelay, interval, participant, reminder)(executionContext)
+  }
 
 }
