258e44a6eac8564e9480b2cf9ceb5ee3a5764fbb
David R. Pugh <drobert.pugh@gmail.com>
2016-02-23 11:52:08 +0000

    Added some constraints to rando trading strategies.

--- src/main/scala-2.11/markets/participants/strategies/RandomLimitOrderTradingStrategy.scala
+++ src/main/scala-2.11/markets/participants/strategies/RandomLimitOrderTradingStrategy.scala
@@ -15,6 +15,19 @@ limitations under the License.
 */
 package markets.participants.strategies
 
+import akka.agent.Agent
 
-trait RandomLimitOrderTradingStrategy extends LimitOrderTradingStrategy with RandomTradingStrategy
+import markets.tickers.Tick
+import markets.tradables.Tradable
+
+import scala.collection.immutable
+
+
+trait RandomLimitOrderTradingStrategy extends LimitOrderTradingStrategy with RandomTradingStrategy {
+
+  def askPrice(ticker: Agent[immutable.Seq[Tick]], tradable: Tradable): Long
+
+  def bidPrice(ticker: Agent[immutable.Seq[Tick]], tradable: Tradable): Long
+
+}
 
