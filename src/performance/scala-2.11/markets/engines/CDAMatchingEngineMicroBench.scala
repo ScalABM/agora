182d3ac73706ebd0d34328b08260746a9bee8dc1
David R. Pugh <drobert.pugh@gmail.com>
2016-04-13 10:13:11 +0100

    Reorg of performance testing source.

--- src/performance/scala-2.11/markets/engines/CDAMatchingEngineMicroBench.scala
+++ src/performance/scala-2.11/markets/engines/CDAMatchingEngineMicroBench.scala
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package engines
+package markets.engines
 
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
--- src/performance/scala-2.11/markets/engines/package.scala
+++ src/performance/scala-2.11/markets/engines/package.scala
@@ -1,2 +1,4 @@
+package markets
+
 /** Suite of performance benchmark tests. */
 package object engines
