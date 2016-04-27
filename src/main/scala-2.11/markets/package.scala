343edcee4158981ecd01e4f72708ac812f78bec6
David R. Pugh <drobert.pugh@gmail.com>
2016-04-28 00:37:52 +0100

    Moved some of the contents to the markets.actors package object.

--- src/main/scala-2.11/markets/package.scala
+++ src/main/scala-2.11/markets/package.scala
@@ -1,3 +1,7 @@
+import akka.actor.ActorRef
+
+import java.util.UUID
+
 /*
 Copyright 2016 David R. Pugh
 
@@ -13,16 +17,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import akka.actor.ActorRef
-import akka.agent.Agent
-
-import java.util.UUID
-
-import markets.orders.Order
-import markets.tickers.Tick
-import markets.tradables.Tradable
-
-
 package object markets {
 
   /** Base trait for all messages. */
@@ -34,7 +28,6 @@ package object markets {
 
   }
 
-
   /** Base trait for representing contracts. */
   trait Contract extends Message {
 
@@ -45,68 +38,5 @@ package object markets {
     def counterparty: Option[ActorRef]
 
   }
-
-  /** Message sent to some `MarketParticipant` actor indicating that the actor should add a
-    * particular market to the collection of markets on which it trades.
-    * @param market
-    * @param ticker
-    * @param timestamp
-    * @param tradable
-    * @param uuid
-    */
-  case class Add(market: ActorRef,
-                 ticker: Agent[Tick],
-                 timestamp: Long,
-                 tradable: Tradable,
-                 uuid: UUID) extends Message
-
-  /** Message sent to some `MarketParticipant` actor indicating that the actor should remove a
-    * particular market from the collection of markets on which it trades.
-    * @param timestamp
-    * @param tradable
-    * @param uuid
-    */
-  case class Remove(timestamp: Long, tradable: Tradable, uuid: UUID) extends Message
-
-  /** Message sent from a `MarketParticipant` actor to some `MarketActor` indicating that it
-    * wishes to cancel a previously submitted order.
-    * @param order
-    * @param timestamp
-    * @param uuid
-    */
-  case class Cancel(order: Order, timestamp: Long, uuid: UUID) extends Message
-
-  /** Message sent from a `MarketActor` to some `MarketParticipant` actor indicating that its
-    * order has been canceled.
-    * @param order
-    * @param timestamp
-    * @param uuid
-    */
-  case class Canceled(order: Order, timestamp: Long, uuid: UUID) extends Message
-
-  /** Message sent from a `MarketActor` to some `MarketParticipant` actor indicating that its
-    * order has been accepted.
-    * @param order
-    * @param timestamp
-    * @param uuid
-    */
-  case class Accepted(order: Order, timestamp: Long, uuid: UUID) extends Message
-
-  /** Message sent from ??? to some `MarketParticipant` actor indicating that a previously
-    * submitted order has been filled.
-    * @param order
-    * @param residual
-    * @param timestamp
-    * @param uuid
-    */
-  case class Filled(order: Order, residual: Option[Order], timestamp: Long, uuid: UUID) extends Message
-
-  /** Message sent from a `MarketActor` to some `MarketParticipant` actor indicating that its
-    * order has been rejected.
-    * @param order
-    * @param timestamp
-    * @param uuid
-    */
-  case class Rejected(order: Order, timestamp: Long, uuid: UUID) extends Message
 
 }
