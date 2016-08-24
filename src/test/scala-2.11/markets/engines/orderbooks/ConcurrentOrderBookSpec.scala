bd031016d08751f0278999cf6fcef663c94f7e18
davidrpugh <david.pugh@kapsarc.org>
2016-08-24 18:31:24 +0300

    Streamlined testing infrastructure.

--- src/test/scala-2.11/markets/engines/orderbooks/ConcurrentOrderBookSpec.scala
+++ src/test/scala-2.11/markets/engines/orderbooks/ConcurrentOrderBookSpec.scala
@@ -1,8 +1,17 @@
 package markets.engines.orderbooks
 
-/**
-  * Created by pughdr on 8/24/2016.
-  */
-class ConcurrentOrderBookSpec {
+import markets.orders.{AskOrder, BidOrder, Order}
+import markets.tradables.Tradable
+
+import scala.util.Random
+
+
+class ConcurrentOrderBookSpec extends AbstractOrderBookSpec {
+
+  val prng = new Random(24)
+
+  def askOrderBookFactory(tradable: Tradable) = ConcurrentOrderBook[AskOrder](tradable)
+
+  def bidOrderBookFactory(tradable: Tradable) = ConcurrentOrderBook[BidOrder](tradable)
 
 }
