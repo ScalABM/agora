09c4d61d9e5caae04e048917cd862ed97d42626a
davidrpugh <david.pugh@kapsarc.org>
2016-09-02 10:52:21 +0300

    Added a trait for generating UUIDs.

--- src/main/scala-2.11/markets/utils/UUIDProvider.scala
+++ src/main/scala-2.11/markets/utils/UUIDProvider.scala
@@ -1,8 +1,38 @@
+/*
+Copyright 2016 ScalABM
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package markets.utils
 
-/**
-  * Created by pughdr on 9/2/2016.
-  */
+import java.util.UUID
+
+
+/** Mixin trait providing functionality to generate UUIDs for objects. */
 trait UUIDProvider {
 
+  /** Provides a randomly generated UUID.
+    *
+    * @return a UUID.
+    */
+  def uuid(): UUID = UUID.randomUUID()
+
+  /** Provides a UUID based on an object's name.
+    *
+    * @param name an Object's name.
+    * @return a UUID.
+    */
+  def uuid(name: String): UUID = UUID.fromString(name)
+
 }
+
