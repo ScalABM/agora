22764b59e338bdcfba8c04847e1b8bc4e9fe8890
davidrpugh <david.pugh@kapsarc.org>
2016-10-06 13:39:38 +0300

    Fixed import error.

--- src/main/scala-2.11/markets/tradables/orders/ask/MarketAskOrder.scala
+++ src/main/scala-2.11/markets/tradables/orders/ask/MarketAskOrder.scala
@@ -17,8 +17,8 @@ package markets.tradables.orders.ask
 
 import java.util.UUID
 
-import markets.tradables.Tradable
 import markets.tradables.orders.bid.BidOrder
+import markets.tradables.Tradable
 
 
 case class MarketAskOrder(issuer: UUID, quantity: Long, timestamp: Long, tradable: Tradable, uuid: UUID)
