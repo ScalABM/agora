2b33d1e252e046798b0d20a70845844411a6ded2
davidrpugh <david.pugh@kapsarc.org>
2016-10-12 04:49:49 +0300

    Started work on performance test.

--- src/performance/scala-2.11/markets/auctions/ContinuousDoubleAuctionMicroBenchmark.scala
+++ src/performance/scala-2.11/markets/auctions/ContinuousDoubleAuctionMicroBenchmark.scala
@@ -1,8 +1,32 @@
+/*
+Copyright 2016 ScalABM
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package markets.auctions
 
-/**
-  * Created by pughdr on 10/11/2016.
-  */
-object ContinuousDoubleAuctionMicroBenchmark {
+import org.scalameter.Bench
 
+import scala.util.Random
+
+
+/** Performance tests for the `ContinuousDoubleAuction` class. */
+object ContinuousDoubleAuctionMicroBenchmark extends Bench.OnlineRegressionReport {
+
+  import markets.RandomOrderGenerator._
+
+  val prng = new Random(42)
+
+  ???
+  
 }
