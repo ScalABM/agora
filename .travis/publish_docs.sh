7f4a8557e0e11ab1210ffb7086f77a83558000f4
davidrpugh <david.pugh@kapsarc.org>
2016-10-16 15:09:08 +0300

    Updated travis scripts.

--- .travis/publish_docs.sh
+++ .travis/publish_docs.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+echo -e "Generating scaladoc.\n"
 
-  echo -e "Generating scaladoc.\n"
+sbt doc
 
-  sbt doc
+echo -e "Publishing scaladoc.\n"
 
-  echo -e "Publishing scaladoc.\n"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ScalABM/markets-sandbox gh-pages > /dev/null
 
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ScalABM/markets-sandbox gh-pages > /dev/null
+cd gh-pages
 
-  cd gh-pages
+# get rid of old docs
+git rm -rf ./docs
 
-  # get rid of old docs
-  git rm -rf ./docs
+# copy over the new docs
+mkdir -p ./docs/api/latest
+cp -Rf ../target/scala-2.11/api/* ./docs/api/latest
 
-  # copy over the new docs
-  mkdir -p ./docs/api/latest
-  cp -Rf ../target/scala-2.11/api/* ./docs/api/latest
+# push to github!
+git add -f .
+git commit -m "Lastest docs for travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git push -fq origin gh-pages > /dev/null
 
-  # push to github!
-  git add -f .
-  git commit -m "Lastest docs for travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+echo -e "Published scaladoc to gh-pages.\n"
 
-  echo -e "Published scaladoc to gh-pages.\n"
-
-fi
