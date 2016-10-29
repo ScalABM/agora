ba57c9e76a48f8d386096edfc055e0e102c52e3d
davidrpugh <david.pugh@kapsarc.org>
2016-10-29 09:59:51 +0300

    Fixed final codacy issues.

--- src/test/scala/org/economicsl/agora/twosided/auctions/continuous/TestPostedPriceAuction.scala
+++ src/test/scala/org/economicsl/agora/twosided/auctions/continuous/TestPostedPriceAuction.scala
@@ -17,11 +17,11 @@ package org.economicsl.agora.twosided.auctions.continuous
 
 import java.util.UUID
 
-import org.economicsl.agora.{orderbooks, Fill}
 import org.economicsl.agora.onesided.auctions.{TestBuyerPostedPriceAuction, TestSellerPostedPriceAuction}
 import org.economicsl.agora.tradables.orders.ask.LimitAskOrder
 import org.economicsl.agora.tradables.orders.bid.LimitBidOrder
 import org.economicsl.agora.tradables.Tradable
+import org.economicsl.agora.{orderbooks, Fill}
 
 import scala.collection.mutable
 
