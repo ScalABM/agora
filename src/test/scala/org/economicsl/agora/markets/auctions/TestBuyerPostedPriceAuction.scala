c7fee8aa5bfba3d6ec2e3de6459ab401eeb615df
davidrpugh <david.pugh@kapsarc.org>
2016-11-01 04:54:18 +0300

    fixed more codacy issues.

--- src/test/scala/org/economicsl/agora/markets/auctions/TestBuyerPostedPriceAuction.scala
+++ src/test/scala/org/economicsl/agora/markets/auctions/TestBuyerPostedPriceAuction.scala
@@ -17,9 +17,9 @@ package org.economicsl.agora.markets.auctions
 
 import org.economicsl.agora.markets.auctions.matching.FindFirstAcceptableOrder
 import org.economicsl.agora.markets.auctions.pricing.BestLimitPricingFunction
-import org.economicsl.agora.markets.tradables.Tradable
 import org.economicsl.agora.markets.tradables.orders.ask.LimitAskOrder
 import org.economicsl.agora.markets.tradables.orders.bid.LimitBidOrder
+import org.economicsl.agora.markets.tradables.Tradable
 import org.economicsl.agora.markets.Fill
 
 
