6b46bb2850225a5180a93fa8e5783bbb1ce4b1ee
davidrpugh <david.pugh@kapsarc.org>
2016-11-17 14:27:39 +0300

    Now have a working simulation.

--- src/functional/scala/org/economicsl/agora/markets/auctions/mutable/continuous/SellersAskDoubleAuctionSimulation.scala
+++ src/functional/scala/org/economicsl/agora/markets/auctions/mutable/continuous/SellersAskDoubleAuctionSimulation.scala
@@ -1,17 +1,98 @@
+/*
+Copyright 2016 ScalABM
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package org.economicsl.agora.markets.auctions.mutable.continuous
 
 import java.util.UUID
 
+import org.economicsl.agora.markets.auctions.matching.FindFirstAcceptableOrder
+import org.economicsl.agora.markets.tradables.{Price, TestTradable, Tradable}
+import org.economicsl.agora.markets.tradables.orders.ask.{LimitAskOrder, PersistentLimitAskOrder}
+import org.economicsl.agora.markets.tradables.orders.bid.{LimitBidOrder, PersistentLimitBidOrder}
+import org.economicsl.agora.markets.tradables.orders.Persistent
+
 import org.apache.commons.math3.analysis.UnivariateFunction
 import org.apache.commons.math3.analysis.solvers.{AllowedSolution, BracketingNthOrderBrentSolver}
-import org.apache.commons.math3.distribution
-import org.economicsl.agora.markets.tradables.{Price, Tradable}
-import org.economicsl.agora.markets.tradables.orders.ask.PersistentLimitAskOrder
-import org.economicsl.agora.markets.tradables.orders.bid.PersistentLimitBidOrder
+import org.apache.commons.math3.{distribution, random, stat}
+
+import scala.util.Random
 
 
 /** Simulation of the continuous 0-Double Auction mechanism of Satterthwaite and Williams (JET, 1989). */
-object SellersAskDoubleAuctionSimulation {
+object SellersAskDoubleAuctionSimulation extends App {
+
+  // Create something to store simulated prices
+  val summaryStatistics = new stat.descriptive.SummaryStatistics()
+
+  // Create a single source of randomness for simulation in order to minimize indeterminacy
+  val seed = 42
+  val prng = new random.MersenneTwister(seed)
+
+  // Create a collection of traders each with it own trading rule
+  val numberTraders = 100
+  val traders = for { i <- 1 to numberTraders } yield UUID.randomUUID()
+
+  // valuation distributions are assumed common knowledge
+  val buyerValuations = new distribution.UniformRealDistribution()
+  val sellerValuations = new distribution.UniformRealDistribution()
+
+  val tradingRules = traders.map { trader =>
+
+    // reservation value is private information
+    val reservationValue = prng.nextDouble()
+
+    if (prng.nextDouble() <= 0.5) {
+      Left(new SellerEquilibriumTradingRule(buyerValuations, trader, reservationValue, sellerValuations))
+    } else {
+      Right(new BuyerEquilibriumTradingRule(buyerValuations, trader, reservationValue, sellerValuations))
+    }
+
+  }
+
+  /** Define a "Seller's Ask" Double Auction. */
+  val auction = {
+
+    val tradable = TestTradable()
+    val askOrderMatchingRule = FindFirstAcceptableOrder[LimitAskOrder, LimitBidOrder with Persistent]()
+    val bidOrderMatchingRule = FindFirstAcceptableOrder[LimitBidOrder, LimitAskOrder with Persistent]()
+    SellersAskDoubleAuction(askOrderMatchingRule, bidOrderMatchingRule, tradable)
+
+  }
+
+  // simple for loop that actually runs a simulation...
+  for { t <- 0 until 1000} {
+
+    // ...generate a batch of orders...this step is trivially parallel!
+    val orders = Random.shuffle(tradingRules).map {
+      case Left(sellerTradingRule) => Left(sellerTradingRule(auction.tradable))
+      case Right(buyerTradingRule) => Right(buyerTradingRule(auction.tradable))
+    }
+
+    // ...feed the orders into the auction mechanism...amount of parallelism in this step varies!
+    val fills = orders.flatMap {
+      case Left(limitAskOrder) => auction.fill(limitAskOrder)
+      case Right(limitBidOrder) => auction.fill(limitBidOrder)
+    }
+
+    // ...collect the generated prices...this step is trivially parallel!
+    fills.foreach(fill => summaryStatistics.addValue(fill.price.value))
+
+  }
+
+  // ...print to screen for reference...
+  println(summaryStatistics.toString)
 
 
   /** Class implementing the equilibrium trading strategy for a buyer participating in a "Seller's Ask" Double Auction.
