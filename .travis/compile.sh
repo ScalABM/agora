d40af47ddaa89d48c683c241c77e1853d8052c0a
davidrpugh <david.pugh@kapsarc.org>
2016-11-21 04:59:15 +0300

    Got rid of troublesome bash scripts.

--- .travis/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# enable error checking...
-set -e
-
-# clean and compile of the project modules using sbt...
-sbt clean compile test:compile functional:compile performance:compile
--- .travis/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# enable error checking!
-set -e
-
-# run tests and generate code coverage report...
-sbt coverage test functional:test
-sbt coverageReport coveralls
