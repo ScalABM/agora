c077c2d088b86f2036179ffcd2116ad61343d544
davidrpugh <david.pugh@kapsarc.org>
2016-11-24 05:28:09 +0300

    Added a discriminatory pricing rule trait.

---
+++ src/main/scala/org/economicsl/agora/markets/auctions/pricing/DiscriminatoryPricingRule.scala
@@ -0,0 +1,23 @@
+/*
+Copyright 2016 ScalABM
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package org.economicsl.agora.markets.auctions.pricing
+
+import org.economicsl.agora.markets.tradables.Price
+import org.economicsl.agora.markets.tradables.orders.{Order, Persistent}
+
+
+/** Trait defining the interface for a discriminatory pricing rule. */
+trait DiscriminatoryPricingRule[-O1 <: Order, -O2 <: Order with Persistent] extends ((O1, O2) => Price)
