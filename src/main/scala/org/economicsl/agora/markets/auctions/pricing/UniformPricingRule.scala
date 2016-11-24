9345fc022d28506133484f0fe7dbc1cec465144f
davidrpugh <david.pugh@kapsarc.org>
2016-11-24 05:12:15 +0300

    Simplified type signature.

--- src/main/scala/org/economicsl/agora/markets/auctions/pricing/UniformPricingRule.scala
+++ src/main/scala/org/economicsl/agora/markets/auctions/pricing/UniformPricingRule.scala
@@ -1,6 +1,20 @@
+/*
+Copyright 2016 ScalABM
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package org.economicsl.agora.markets.auctions.pricing
 
-import org.economicsl.agora.markets.auctions.mutable.orderbooks.{AskOrderBook, BidOrderBook}
 import org.economicsl.agora.markets.tradables.Price
 import org.economicsl.agora.markets.tradables.orders.Persistent
 import org.economicsl.agora.markets.tradables.orders.ask.AskOrder
@@ -8,6 +22,5 @@ import org.economicsl.agora.markets.tradables.orders.bid.BidOrder
 
 
 /** Trait defining a uniform pricing rule. */
-trait UniformPricingRule[A <: AskOrder with Persistent, AB <: AskOrderBook[A],
-                         B <: BidOrder with Persistent, BB <: BidOrderBook[B]]
-  extends ((AB, BB) => Price)
+trait UniformPricingRule[A <: AskOrder with Persistent, B <: BidOrder with Persistent]
+  extends ((OrderBook[A], OrderBook[B]) => Price)
